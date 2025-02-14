e8c96541014bc9dbeee5a28c18db5b11138b44c9
AnnaShiba <102438674+AnnaShiba@users.noreply.github.com>
2025-02-13 21:44:59 -0500

    update graphql schema to include employees

--- api/schema.js
+++ api/schema.js
@@ -12,8 +12,8 @@ type User {
     username: String!
     email: String!
     password: String!
-    created_at: Date
-    updated_at: Date
+    created_at: String
+    updated_at: String
 }
 
 input UserInput {
@@ -22,13 +22,46 @@ input UserInput {
     password: String!
 }
 
+type Employee {
+    _id: String
+    email: String!
+    first_name: String!
+    last_name: String!
+    gender: Gender!
+    designation: String!
+    salary: Float!
+    date_of_joining: String!
+    department: String!
+    employee_photo: String
+    created_at: String
+    updated_at: String
+}
+
+input EmployeeInput {
+    email: String!
+    first_name: String!
+    last_name: String!
+    gender: Gender!
+    designation: String!
+    salary: Float!
+    date_of_joining: String!
+    department: String!
+    employee_photo: String
+}
+
 
 type Query {
     login(username: String!, password: String!): User
+    getAllEmployees: [Employee]
+    getEmployee(id: String!): Employee
+    searchEmployees(query: String!): [Employee]
 }
 
 type Mutation {
     signup(user: UserInput!): User
+    addEmployee(employee: EmployeeInput!): Employee
+    updateEmployee(id: String!, employee: EmployeeInput!): Employee
+    deleteEmployee(id: String!): Boolean
 }
 `
 
