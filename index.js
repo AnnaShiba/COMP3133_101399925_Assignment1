8fcb6f58dbcfb960ea6bfe8b64117d625aa9eb1d
AnnaShiba <102438674+AnnaShiba@users.noreply.github.com>
2025-02-13 21:10:14 -0500

    set up apollo server

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+const { ApolloServer } = require('apollo-server-express');
+const resolvers = require('./api/resolvers');
+const schema = require('./api/schema');
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('*', cors());
+
+const apolloServer = new ApolloServer({ typeDefs: schema, resolvers });
 
 mongoose.connect(process.env.MONGODB_URI, {})
-    .then(_ => server.listen(3123, () => console.log("Server ready on port 3123 with MongoDB and Socket.io.")))
+    .then(_ => app.listen(4000, () => {
+        apolloServer.start().then(_ => {
+            apolloServer.applyMiddleware({ app });
+            console.log("Server ready on port 4000 with MongoDB and Apollo Server.");
+        });
+    }))
     .catch(error => console.log(error));
     
